f9dd5fb1c86d113ed979dce087658be43d8340ed
Paul Bauer <pbauer@colligo.com>
2015-11-18 16:04:18 -0800

    initial commit

---
+++ DefaultAspNetApp/DefaultAspNetApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DefaultAspNetApp.MvcApplication" Language="C#" %>
---
+++ DefaultAspNetApp/DefaultAspNetApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DefaultAspNetApp.Startup))]
+namespace DefaultAspNetApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
